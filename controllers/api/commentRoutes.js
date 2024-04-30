3f6b9c74e43fc0acb13a372e1c1af504704caf47
Katie Bug <katierosealford@gmail.com>
2024-04-30 18:40:54 -0400

    added more routes, still not sure if i need to find specific by findbybpk or what I have written, a work in progress

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -3,6 +3,20 @@ const router = require("express").Router();
 const { Comment } = require("../../models");
 
 
+// Create a new comment
+router.post("/", async (req, res) => {
+    try {
+        const dbCommentData = await Comment.create({
+            comment_text: req.body.comment_text,
+            post_id: req.body.post_id,
+            user_id: req.session.user_id,
+        });
+        res.status(200).json(dbCommentData);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ message: "Request to create a new comment failed!" });
+    }
+});
 
 // Export the router
 module.exports = router;
--- controllers/homepageRoutes.js
+++ controllers/homepageRoutes.js
@@ -2,6 +2,72 @@
 const router = require("express").Router();
 const { Post, User, Comment } = require("../models");
 
+// Route to render homepage
+router.get("/", async (req, res) => {
+    try {
+        const dbPostData = await Post.findAll({
+            attributes: ["id", "title", "content", "created_at"],
+            include: [
+                {
+                    model: User,
+                    attributes: ["username"],
+                },
+                {
+                    model: Comment,
+                    attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+                    include: {
+                        model: User,
+                        attributes: ["username"],
+                    },
+                },
+            ],
+        });
+
+        const posts = dbPostData.map((post) => post.get({ plain: true }));
+
+        res.render("homepage", { posts });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ message: "Request for all posts from user unable to be fulfilled, posts not found!" });
+    }
+});
+
+// Route to render individual post page
+router.get("/post/:id", async (req, res) => {
+    try {
+        const dbPostData = await Post.findOne({
+            where: { id: req.params.id },
+            attributes: ["id", "title", "content", "created_at"],
+            include: [
+                {
+                    model: User,
+                    attributes: ["username"],
+                },
+                {
+                    model: Comment,
+                    attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+                    include: {
+                        model: User,
+                        attributes: ["username"],
+                    },
+                },
+            ],
+        });
+
+        if (!dbPostData) {
+            res.status(404).json({ message: "Request for post unable to be fulfilled, post not found!" });
+            return;
+        }
+
+        const post = dbPostData.get({ plain: true });
+
+        res.render("single-post", { post });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ message: "Request for post unable to be fulfilled, post not found!" });
+    }
+});
+
 
 
 // module exports router
