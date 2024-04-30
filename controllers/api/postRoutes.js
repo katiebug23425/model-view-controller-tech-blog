e1af58b7cfe657edcb4cf394dca0681ee4423898
Katie Bug <katierosealford@gmail.com>
2024-04-30 16:38:31 -0400

    added get all posts and get specific post but not sure if they are formatted properly. The get post by id may need to be findbypk instead of findone? looking into it

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,7 +1,69 @@
 const router = require("express").Router();
 const { Post, User, Comment } = require("../../models");
 
+// Get all posts with associated username
+router.get("/", async (req, res) => {
+    try {
+      const dbPostData = await Post.findAll({
+        attributes: ["id", "title", "content", "created_at"],
+        include: [
+          {
+            model: User,
+            attributes: ["username"],
+          },
+          {
+            model: Comment,
+            attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+            include: {
+              model: User,
+              attributes: ["username"],
+            },
+          },
+        ],
+      });
+  
+      res.json(dbPostData);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({ message: "Request for all posts from user unable to be fulfilled, posts not found!" });
+    }
+  });
 
+  // Get single post by ID with associated username and comments
+router.get("/:id", async (req, res) => {
+    try {
+      const dbPostData = await Post.findOne({
+        where: { id: req.params.id },
+        attributes: ["id", "title", "content", "created_at"],
+        include: [
+          {
+            model: User,
+            attributes: ["username"],
+          },
+          {
+            model: Comment,
+            attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+            include: {
+              model: User,
+              attributes: ["username"],
+            },
+          },
+        ],
+      });
+  
+      if (!dbPostData) {
+        res.status(404).json({ message: "Request for post unable to be fulfilled, post not found!" });
+        return;
+      }
+  
+      res.json(dbPostData);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({ message: "Request for post unable to be fulfilled, post not found!" });
+    }
+  });
+
+  
 
 
 
