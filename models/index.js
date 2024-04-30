4e551bcdf44bc70e8eab3c653220a776adcc9611
Katie Bug <katierosealford@gmail.com>
2024-04-30 15:19:56 -0400

    added imports to all models, created user model, started index.js code

---
+++ models/index.js
@@ -0,0 +1,15 @@
+// Import the necessary models
+const User = require("./user");
+const Post = require("./post");
+const Comment = require("./comment");
+
+
+
+
+
+
+
+
+
+// Export the models
+module.exports = { User, Post, Comment };
