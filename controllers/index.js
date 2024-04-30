4b5dddc1cceb06948976cdded9f4291534237ce4
Katie Bug <katierosealford@gmail.com>
2024-04-30 18:55:42 -0400

    added comments to index.js in controllers, added post and commentr models

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,9 @@
+// imports
 const router = require("express").Router();
 const homepageRoutes = require("./homepageRoutes");
 const apiRoutes = require("./api");
 
-
+// set up routes
 router.use("/", homepageRoutes);
 router.use("/api", apiRoutes);
 
