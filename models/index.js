7f8d76c7c6fedd3f9c234910e8d3f9f498d5ed62
Katie Bug <katierosealford@gmail.com>
2024-04-30 19:11:33 -0400

    defined relationships  in index.jsand added scripts to package.json file

--- models/index.js
+++ models/index.js
@@ -3,13 +3,30 @@ const User = require("./user");
 const Post = require("./post");
 const Comment = require("./comment");
 
+// define model relationships
+User.hasMany(Post, {
+    foreignKey: "user_id",
+});
 
+Post.belongsTo(User, {
+    foreignKey: "user_id",
+});
 
+User.hasMany(Comment, { 
+    foreignKey: "user_id",
+});
 
+Comment.belongsTo(User, {
+    foreignKey: "user_id",
+});
 
+Post.hasMany(Comment, {
+    foreignKey: "post_id",
+});
 
-
-
+Comment.belongsTo(Post, {
+    foreignKey: "post_id",
+});
 
 // Export the models
 module.exports = { User, Post, Comment };
