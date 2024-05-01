aee28992d7ada5b666ec5b7865b489eb47ba6e0e
Katie Bug <katierosealford@gmail.com>
2024-04-30 21:18:10 -0400

    set up handlebars in server.js file but not sure if I did it right

--- server.js
+++ server.js
@@ -5,24 +5,37 @@ const SequelizeStore = require("connect-session-sequelize")(session.Store);
 const routes = require("./controllers");
 const sequelize = require("./config/connection");
 const exphbs = require("express-handlebars");
-
-
-
-
+const hbs = exphbs.create({ helpers: require("./utils/helpers") });
 
 // Creating express app and setting port
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
 
-
+app.use(session(sess));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 
-
-
-
+app.use(
+  session({
+    secret: process.env.SECRET,
+    store: new SequelizeStore({ db: sequelize }),
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 
 // Using routes from controller
 app.use(routes);
