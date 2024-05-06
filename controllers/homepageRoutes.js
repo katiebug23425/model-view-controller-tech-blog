ec0d70829c46892991ce5b747af312b963ed685e
Katie Bug <katierosealford@gmail.com>
2024-05-06 10:41:33 -0400

    updated so when logged in you can access dashboard but still cannot get my posts when created

--- controllers/homepageRoutes.js
+++ controllers/homepageRoutes.js
@@ -77,7 +77,7 @@ router.get("/dashboard", withAuth, async (req, res) => {
     try {
         const dbPostData = await Post.findAll({
             where: { user_id: req.session.user_id },
-            attributes: ["id", "title", "post_text", "created_at"],
+            attributes: ["id", "title", "content", "created_at"],
             include: [
                 {
                     model: User,
@@ -137,7 +137,7 @@ router.get("/login", (req, res) => {
 router.get("/edit/:id", async (req, res) => {
     try {
         const dbPostData = await Post.findByPk(req.params.id, {
-            attributes: ["id", "title", "post_text", "created_at"],
+            attributes: ["id", "title", "content", "created_at"],
             include: [
                 {
                     model: User,
