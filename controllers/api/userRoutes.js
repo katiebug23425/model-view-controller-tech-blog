c658006a87be3bbd4d08150f16576c85dbd26e96
Katie Bug <katierosealford@gmail.com>
2024-05-06 17:59:13 -0400

    testing to fix login & logout errors

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -49,7 +49,7 @@ router.post("/login", async (req, res) => {
         return;
       }
   
-      const validPassword = dbUserData.checkPassword(req.body.password);
+      const validPassword = await dbUserData.checkPassword(req.body.password);
   
       if (!validPassword) {
         res
